60198e3163e4b00f5eb5422873c7c826971a1e69
NativAmar <89576789+NativAmar@users.noreply.github.com>
2021-08-29 19:59:48 +0300

    first

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-const num = 123
-let sum = 0
-
-console.log(sum)
+const num = 123;
+let sum = 0;
+let first=num%10;;
+let second=Math.floor(num/10) %10;
+let third=Math.floor(num/100);
+sum=third+second+first;
+console.log(sum);
